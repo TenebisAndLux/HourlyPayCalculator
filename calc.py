5e1908ce640affd206e44e3f0da1485e99ef5659
Averkin Mathew <meazerg@gmail.com>
2024-04-24 00:45:54 +0300

    up

--- calc.py
+++ calc.py
@@ -1,16 +1,38 @@
+import re
+import math
+
+
 def calculate_work_hours(time_str):
-    hours, minutes = map(int, time_str.split(':'))
-    return hours + minutes / 60
+    pattern = r'^(?P<hours>\d+):(?P<minutes>\d+)$'
+    match = re.match(pattern, time_str)
+    if match:
+        return float(match.group('hours')) + float(match.group('minutes')) / 60
+    else:
+        print("Неверный формат времени.")
+        return 0
 
 
 def calculate_monthly_hours():
     total_hours = 0
+    previous_time = None
     while True:
         time_str = input("Введите время работы (через двоеточие, например 4:30), "
-                        "для завершения введите 'exit': ").lower()
+                         "для завершения введите 'exit' или 'del' для удаления предыдущего значения: ").lower()
         if time_str == 'exit':
             break
-        total_hours += calculate_work_hours(time_str)
+        elif time_str == 'del':
+            if previous_time is not None:
+                total_hours -= previous_time
+                previous_time = None
+            else:
+                print("Нет предыдущих значений для удаления.")
+        else:
+            try:
+                time_hours = calculate_work_hours(time_str)
+                total_hours += time_hours
+                previous_time = time_hours
+            except ValueError:
+                print("Неверный формат времени.")
     return total_hours
 
 
@@ -29,4 +51,3 @@ if __name__ == '__main__':
     print("Общее количество отработанных часов за месяц:", total_monthly_hours)
     print("Общая сумма заработной платы:", total_salary)
     print("Сумма заработной платы после удержания налогов:", total_salary_tax)
-
