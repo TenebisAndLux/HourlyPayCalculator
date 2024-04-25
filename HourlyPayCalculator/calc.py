ef9f6cf512d285129b888f4f0771e350f998f315
Averkin Mathew <meazerg@gmail.com>
2024-04-25 23:50:59 +0300

    application with installer

---
+++ HourlyPayCalculator/calc.py
@@ -0,0 +1,54 @@
+import re
+
+
+def calculate_work_hours(time_str):
+    """Преобразует время в формате "ЧЧ:ММ" в количество часов"""
+    pattern = r'^(?P<hours>\d+):(?P<minutes>\d+)$'
+    match = re.match(pattern, time_str)
+    if match:
+        return float(match.group('hours')) + float(match.group('minutes')) / 60
+    else:
+        raise ValueError("Неверный формат времени")
+
+
+def load_data_from_file():
+    """Загружает данные из файла 'work_hours.txt' и возвращает список их значений"""
+    try:
+        with open('data/work_hours.txt', 'r') as f:
+            return [float(line.strip()) for line in f]
+    except FileNotFoundError:
+        return []
+
+
+def save_data_to_file(data):
+    """Сохраняет данные в файл 'work_hours.txt'"""
+    with open('data/work_hours.txt', 'w') as f:
+        for value in data:
+            f.write(f'{value:.2f}\n')
+
+
+def add_time(data, time_str):
+    """Добавляет время в формате "ЧЧ:ММ" в список значений"""
+    try:
+        data.append(calculate_work_hours(time_str))
+        save_data_to_file(data)
+        return "Значение добавлено."
+    except ValueError:
+        return "Неверный формат времени."
+
+
+def calculate_monthly_hours(data, hourly_rate=625):
+    """Посчитывает общее количество часов за месяц,
+    общую сумму заработной платы и сумму заработной платы после удержания налогов"""
+    total_monthly_hours = sum(data)
+    count = len(data)
+    if count:
+        return (
+                f"Общее количество отработанных часов за месяц: {total_monthly_hours:.2f}\n" +
+                f"Общая сумма заработной платы: {total_monthly_hours * hourly_rate:.2f}\n" +
+                f"Сумма заработной платы после удержания налогов: {total_monthly_hours * hourly_rate * 0.87:.2f}\n" +
+                f"Дней отработано: {count}\n" +
+                f"Часов в день: {total_monthly_hours / count:.2f}"
+        )
+    else:
+        return "Для расчета необходимо добавить значения."
--- calc.py
+++ calc.py
@@ -2,82 +2,53 @@ import re
 
 
 def calculate_work_hours(time_str):
+    """Преобразует время в формате "ЧЧ:ММ" в количество часов"""
     pattern = r'^(?P<hours>\d+):(?P<minutes>\d+)$'
     match = re.match(pattern, time_str)
-
     if match:
         return float(match.group('hours')) + float(match.group('minutes')) / 60
     else:
-        raise ValueError("Неверный формат времени.")
+        raise ValueError("Неверный формат времени")
 
 
 def load_data_from_file():
+    """Загружает данные из файла 'work_hours.txt' и возвращает список их значений"""
     try:
-        with open('work_hours.txt', 'r') as f:
+        with open('data/work_hours.txt', 'r') as f:
             return [float(line.strip()) for line in f]
     except FileNotFoundError:
         return []
 
 
 def save_data_to_file(data):
-    with open('work_hours.txt', 'w') as f:
+    """Сохраняет данные в файл 'work_hours.txt'"""
+    with open('data/work_hours.txt', 'w') as f:
         for value in data:
-            f.write(f'{value}\n')
+            f.write(f'{value:.2f}\n')
 
 
-def add_time(data):
-    while True:
-        time_str = input("Введите время работы (через двоеточие, например 4:30) или 'exit' для выхода: ")
-        if time_str == 'exit':
-            break
-        else:
-            try:
-                data.append(calculate_work_hours(time_str))
-                save_data_to_file(data)
-                print("Значение добавлено.")
-            except ValueError:
-                print("Неверный формат времени.")
+def add_time(data, time_str):
+    """Добавляет время в формате "ЧЧ:ММ" в список значений"""
+    try:
+        data.append(calculate_work_hours(time_str))
+        save_data_to_file(data)
+        return "Значение добавлено."
+    except ValueError:
+        return "Неверный формат времени."
 
 
-def calculate_monthly_hours(data):
+def calculate_monthly_hours(data, hourly_rate=625):
+    """Посчитывает общее количество часов за месяц,
+    общую сумму заработной платы и сумму заработной платы после удержания налогов"""
     total_monthly_hours = sum(data)
     count = len(data)
     if count:
-        print("Общее количество отработанных часов за месяц:", total_monthly_hours)
-        print("Общая сумма заработной платы:", total_monthly_hours * 625)
-        print("Сумма заработной платы после удержания налогов:", total_monthly_hours * 625 * 0.87)
-        print("Дней отработано:", count)
-        print("Часов в день:", total_monthly_hours / count)
+        return (
+                f"Общее количество отработанных часов за месяц: {total_monthly_hours:.2f}\n" +
+                f"Общая сумма заработной платы: {total_monthly_hours * hourly_rate:.2f}\n" +
+                f"Сумма заработной платы после удержания налогов: {total_monthly_hours * hourly_rate * 0.87:.2f}\n" +
+                f"Дней отработано: {count}\n" +
+                f"Часов в день: {total_monthly_hours / count:.2f}"
+        )
     else:
-        print("Для расчета необходимо добавить значения.")
-
-
-def main_input(data):
-    while True:
-        command = input(
-            "Введите 'add' для добавления времени, 'calculate' для расчета, 'del' для удаления, 'clear' для очистки файла или 'exit' для выхода: ").lower()
-
-        if command == 'add':
-            add_time(data)
-        elif command == 'calculate':
-            calculate_monthly_hours(data)
-        elif command == 'del':
-            if data:
-                data.pop()
-                save_data_to_file(data)
-                print("Последнее значение удалено.")
-            else:
-                print("Нет значений для удаления.")
-        elif command == 'clear':
-            data = []
-            save_data_to_file(data)
-            print("Файл очищен.")
-        elif command == 'exit':
-            break
-        else:
-            print("Неизвестная команда.")
-
-
-if __name__ == '__main__':
-    data = load_data_from_file()
-    main_input(data)
+        return "Для расчета необходимо добавить значения."
