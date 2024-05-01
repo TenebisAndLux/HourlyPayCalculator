05ec98afbbca3f15c07805755ead06beda9b63ae
Averkin Mathew <meazerg@gmail.com>
2024-05-01 15:37:30 +0300

    up

---
+++ style.py
@@ -0,0 +1,43 @@
+stylesheet = """
+    QWidget {
+        background-color: #3A6E53;
+        color: #F2F2F2;
+        font-size: 24px;
+        border: 2px solid #579D01;
+        border-radius: 15px;
+        box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
+    }
+
+    QFrame {
+        background-color: #3A6E53;
+        border: 2px solid #579D01;
+        border-radius: 10px;
+        box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
+    }
+
+    QLabel {
+        font-size: 24px;
+        font-weight: bold;
+    }
+
+    QLineEdit {
+        background-color: #579D01; 
+        color: #F2F2F2; 
+        font-size: 24px; 
+        border: none; 
+        border-radius: 10px;                   
+        padding: 10px;
+    }
+
+    QPushButton {
+        background-color: #579D01; 
+        color: #F2F2F2; 
+        font-size: 24px; 
+        border: none; 
+        border-radius: 10px;                     
+        padding: 10px;
+    }
+
+    QTextEdit {background-color: #579D01; color: #F2F2F2; font-size: 24px; border: none; border-radius: 10px;                   padding: 10px;}
+"""
+
